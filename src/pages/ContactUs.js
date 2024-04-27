4c5dbeee00b9ac3155e06488bb7e44652f8feb8e
eswar <eswarmarcharla143@gmail.com>
2024-04-27 20:59:16 +0530

    changed contact file

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -6,9 +6,9 @@ const ContactUs = () => {
       <h1>Contact Us</h1>
       <p>Please feel free to contact us using the information below:</p>
       <ul>
-        <li>Email: example@example.com</li>
-        <li>Phone: +1234567890</li>
-        <li>Address: 123 Main Street, City, Country</li>
+        <li>Email: eswarmarcharla143@gmail.com</li>
+        <li>Phone: 9618074482</li>
+        <li>Address: JC Main Street, visakhapatnam,India</li>
       </ul>
     </div>
   );
